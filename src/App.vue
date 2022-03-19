2e90ce0a7dbbd6745183f930e4133297179c4c1c
Sara Barros <sarasgnt@gmail.com>
2022-03-18 23:19:00 -0300

    componente do app top bar

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,21 @@
 <template>
   <v-app>
-    <v-app-bar color="green lighten-1" app dark>
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Campeonato Brasileiro ⚽</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-    </v-app-bar>
-
-    <v-navigation-drawer v-model="drawer" absolute temporary>
-      <v-list nav dense>
-        <v-list-item-group
-          v-model="group"
-          active-class="green darken-2"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Inicio</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-soccer</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Classificação</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-    <v-main> 
-        <MeuInicio/>
+    <AppTopBar />
+    <v-main>
+      <MeuInicio />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import MeuInicio from './components/MeuInicio'
+import MeuInicio from "./components/MeuInicio";
+import AppTopBar from "./components/shared/AppTopBar.vue";
 export default {
   name: "App",
 
-  data: () => ({
-    drawer: false,
-    group:null,
-  }),
   components: {
-    MeuInicio
-  }
+    MeuInicio,
+    AppTopBar,
+  },
 };
 </script>
